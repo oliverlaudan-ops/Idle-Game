a153aeb3e6fdbece017b2e7257cd9a7cf1c2d0bf
oliverlaudan-ops <oliver.laudan@gmail.com>
2025-11-20 12:20:06 +0100

    Update resource.js

--- resource.js
+++ resource.js
@@ -1,1 +1,27 @@
+// resource.js
+
+export class Resource {
+  constructor(id, name, icon, rpc = 1, rps = 0, unlocked = true){
+    this.id = id;
+    this.name = name;
+    this.icon = icon;
+    this.amount = 0;
+    this.rpc = rpc;    // Ressourcen pro Klick
+    this.rps = rps;    // Ressourcen pro Sekunde
+    this.unlocked = unlocked;
+  }
+
+  add(n){
+    // Beachte: Der Prestige-Bonus wird von der Spiel-Logik (im Game) beim Aufruf add() ergänzt!
+    this.amount += n;
+  }
+
+  spend(n){
+    if (this.amount >= n){
+      this.amount -= n;
+      return true;
+    }
+    return false;
+  }
+}
 
