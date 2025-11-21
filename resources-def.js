0362a8a53f87d65b3bb3439d2dd99914571b0278
oliverlaudan-ops <oliver.laudan@gmail.com>
2025-11-21 06:43:41 +0100

    Create resources-def.js

---
+++ resources-def.js
@@ -0,0 +1,14 @@
+// resources-def.js
+import Resource from './resource.js';
+
+// Alle Ressourcen als Array definieren
+const resourcesList = [
+  new Resource('stein', 'Stein', '🪨', 1, 0, true),      // Startressource
+  new Resource('holz',  'Holz',  '🌲', 0, 0, false),     // Wird freigeschaltet
+  new Resource('metall','Metall','⛏️', 0, 0, false),    // Wird freigeschaltet
+  new Resource('kristall','Kristall','💎', 0, 0, false)  // Wird freigeschaltet
+  // Hier kannst du beliebig neue Ressourcen ergänzen:
+  // new Resource('kupfer', 'Kupfer', '🪙', 0, 0, false)
+];
+
+export default resourcesList;
