4933506076d665ff8bb4b28b8b0b67374bbaeb0a
oliverlaudan-ops <oliver.laudan@gmail.com>
2025-11-21 08:51:50 +0100

    Update resources-def.js

--- resources-def.js
+++ resources-def.js
@@ -5,8 +5,10 @@ import { Resource } from './resource.js';
 const resourcesList = [
   new Resource('stein', 'Stein', '🪨', 1, 0, true),      // Startressource
   new Resource('holz',  'Holz',  '🌲', 0, 0, false),     // Wird freigeschaltet
+  new Resource('ton', 'Ton', '🧱', 0, 0, false) // <--- NEU
   new Resource('metall','Metall','⛏️', 0, 0, false),    // Wird freigeschaltet
   new Resource('kristall','Kristall','💎', 0, 0, false)  // Wird freigeschaltet
+ 
   // Hier kannst du beliebig neue Ressourcen ergänzen:
   // new Resource('kupfer', 'Kupfer', '🪙', 0, 0, false)
 ];
