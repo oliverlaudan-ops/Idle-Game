c6ed78e9d7555118d88c7833332c456a85e8d17e
oliverlaudan-ops <oliver.laudan@gmail.com>
2025-12-08 13:40:47 +0100

    Update upgrades-def.js

--- upgrades-def.js
+++ upgrades-def.js
@@ -66,13 +66,13 @@ const upgradesList = [
   new Upgrade({
     id: "stein_research_rpc",
     name: "Stein-Forschung",
-    desc: "Erhöht Stein pro Klick um 10% deiner aktuellen Stein-pro-Sekunde-Rate.",
+    desc: "Erhöht Stein pro Klick um 25% deiner aktuellen Stein-pro-Sekunde-Rate.",
     costRes: "stein",
-    costBase: 500,
-    costMult: 1.6,
+    costBase: 200,
+    costMult: 1.5,
     apply: (game) => {
       const r = game.getResource("stein");
-      r.rpc += r.rps * 0.10;
+      r.rpc += r.rps * 0.25;
     },
     research: true
   }),
@@ -153,13 +153,13 @@ const upgradesList = [
   new Upgrade({
     id: "holz_research_rpc",
     name: "Holz-Forschung",
-    desc: "Erhöht Holz pro Klick um 10% deiner aktuellen Holz-pro-Sekunde-Rate.",
+    desc: "Erhöht Holz pro Klick um 25% deiner aktuellen Holz-pro-Sekunde-Rate.",
     costRes: "holz",
     costBase: 500,
     costMult: 1.6,
     apply: (game) => {
       const r = game.getResource("holz");
-      r.rpc += r.rps * 0.10;
+      r.rpc += r.rps * 0.25;
     },
     research: true
   }),
@@ -240,13 +240,13 @@ const upgradesList = [
   new Upgrade({
     id: "ton_research_rpc",
     name: "Ton-Forschung",
-    desc: "Erhöht Ton pro Klick um 10% deiner aktuellen Ton-pro-Sekunde-Rate.",
+    desc: "Erhöht Ton pro Klick um 20% deiner aktuellen Ton-pro-Sekunde-Rate.",
     costRes: "ton",
-    costBase: 500,
-    costMult: 1.6,
+    costBase: 1000,
+    costMult: 1.7,
     apply: (game) => {
       const r = game.getResource("ton");
-      r.rpc += r.rps * 0.10;
+      r.rpc += r.rps * 0.20;
     },
     research: true
   }),
@@ -339,13 +339,13 @@ const upgradesList = [
   new Upgrade({
     id: "metall_research_rpc",
     name: "Metall-Forschung",
-    desc: "Erhöht Metall pro Klick um 10% deiner aktuellen Metall-pro-Sekunde-Rate.",
+    desc: "Erhöht Metall pro Klick um 15% deiner aktuellen Metall-pro-Sekunde-Rate.",
     costRes: "metall",
-    costBase: 500,
-    costMult: 1.6,
+    costBase: 5000,
+    costMult: 1.8,
     apply: (game) => {
       const r = game.getResource("metall");
-      r.rpc += r.rps * 0.10;
+      r.rpc += r.rps * 0.15;
     },
     research: true
   }),
@@ -439,8 +439,8 @@ const upgradesList = [
     name: "Kristall-Forschung",
     desc: "Erhöht Kristall pro Klick um 10% deiner aktuellen Kristall-pro-Sekunde-Rate.",
     costRes: "kristall",
-    costBase: 500,
-    costMult: 1.6,
+    costBase: 10000,
+    costMult: 1.9,
     apply: (game) => {
       const r = game.getResource("kristall");
       r.rpc += r.rps * 0.10;
